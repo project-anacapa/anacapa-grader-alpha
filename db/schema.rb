3a5a5fc2c0b2024b6fd7edc8ba5285347ab4da5c
Phill Conrad <pconrad@cs.ucsb.edu>
2016-04-26 15:29:43 -0700

    WIP: login/logout works.  Testing not sure yet

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160422234854) do
+ActiveRecord::Schema.define(version: 20160426222730) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160422234854) do
     t.string   "provider"
     t.string   "uid"
     t.string   "github_token"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
