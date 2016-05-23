c57e55e29fe8adbd8cd046ce634c31f0cfd82dd4
David Acevedo (Intern) <david.acevedo@citrix.com>
2016-05-23 14:23:19 -0700

    Add selenium tests

--- spec/features/admin_page_spec.rb
+++ spec/features/admin_page_spec.rb
@@ -2,19 +2,22 @@ require 'rails_helper.rb'
 
 describe "the admin panel view", :type => :feature do
   before do
-    Rails.application.env_config["devise.mapping"] = Devise.mappings[:user]
-    Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:github]
+    @user = FactoryGirl.create(:admin)
+    ApplicationController.any_instance.stub(:current_user).and_return(@user)
+    login_as(@user, :scope => :user)
   end
-  it "should go back to the homepage if not an admin" do
+
+  after do
+    logout(@user)
+  end
+
+  it "should go to the admin page if you are an admin" do
+    visit '/admin_panel'
+    expect(page).to have_content 'Welcome, Admin! User Email User Name Administrator Instructor User ID test@example.com Test Name'
+  end
+  it "should go back to the homepage if you are not an admin" do
+    @user.remove_role :admin
     visit '/admin_panel'
     expect(page).to have_content 'Coming Soon!'
-  end
-  it "should go back to the homepage when not logged in as an admin" do
-    visit '/'
-    #stub TODO write this test
-  end
-  it "should say 'Welcome, Admin!' when logged in as an admin" do
-    visit '/'
-    #stub TODO write this test
   end
 end
