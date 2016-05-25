8f1c8c1793c5ec9b4821fee9468bf0d7d1a3f1f7
David Acevedo (Intern) <david.acevedo@citrix.com>
2016-05-24 19:10:19 -0700

    Add more tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -9,4 +9,10 @@ FactoryGirl.define do
     end
   end
 
+  # factory :user do
+  #   name 'Test2'
+  #   email 'test2@example.com'
+  #   id '22222'
+  # end
+
 end
--- spec/features/admin_page_spec.rb
+++ spec/features/admin_page_spec.rb
@@ -1,23 +1,38 @@
 require 'rails_helper.rb'
 
-describe "the admin panel view", :type => :feature do
+describe 'the admin panel view', :type => :feature do
   before do
     @user = FactoryGirl.create(:admin)
     ApplicationController.any_instance.stub(:current_user).and_return(@user)
+    @user.save
     login_as(@user, :scope => :user)
+
   end
 
   after do
     logout(@user)
   end
 
-  it "should go to the admin page if you are an admin" do
+  it 'should go to the admin page if you are an admin' do
     visit '/admin_panel'
-    expect(page).to have_content 'Welcome, Admin! User Email User Name Administrator Instructor User ID test@example.com Test Name'
+    expect(page).to have_css('body > h1', :text => "Welcome, Admin!")
   end
-  it "should go back to the homepage if you are not an admin" do
+  it 'should go back to the homepage if you are not an admin' do
     @user.remove_role :admin
     visit '/admin_panel'
-    expect(page).to have_content 'Coming Soon!'
+    expect(page).to have_css('body > p', :text => "Coming Soon!")
+  end
+
+  it 'should toggle correctly into non-admin' do
+    visit '/admin_panel'
+    expect(page).to have_css('#admin_toggle_12345')
+    find(:css, '#admin_toggle_12345').set(false)
+    page.has_checked_field?('#admin_toggle_12345').should be false
+  end
+
+  it 'should toggle correctly into admin' do
+    visit '/admin_panel'
+    expect(page).to have_css('#admin_toggle_12345')
+    page.has_checked_field?('#admin_toggle_12345').should be true
   end
 end
