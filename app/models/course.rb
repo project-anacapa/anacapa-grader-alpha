92d36e5ad3793619cdf5184cc52ba49fd39e7e23
Baby Pepper <royxagain@gmail.com>
2016-06-07 11:20:05 -0700

    pass the test of return format of a course

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,9 @@ class Course < ActiveRecord::Base
   validates :num, :presence => true
   validates :desc, :presence => true
   validates :term, :presence => true
+
+  def course_slug
+    return [dept, num, term].join "_"
+  end
+
 end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -6,16 +6,23 @@ RSpec.describe Course, type: :model do
   it "has a valid factory" do
     FactoryGirl.create(:course).should be_valid
   end
-  it "is invalid without a deptartment" do
-    FactoryGirl.build(:course, dept: nil).should_not be_valid
+  describe "invalid fields" do
+    it "is invalid without a deptartment" do
+      FactoryGirl.build(:course, dept: nil).should_not be_valid
+    end
+    it "is invalid without a number" do
+      FactoryGirl.build(:course, num: nil).should_not be_valid
+    end
+    it "is invalid without a description" do
+      FactoryGirl.build(:course, desc: nil).should_not be_valid
+    end
+    it "is invalid without a term" do
+      FactoryGirl.build(:course, term: nil).should_not be_valid
+    end
   end
-  it "is invalid without a number" do
-    FactoryGirl.build(:course, num: nil).should_not be_valid
+
+  it "should return the course in correct format" do
+    FactoryGirl.create(:course).course_slug.should == "Computer Science_170_S16"
   end
-  it "is invalid without a description" do
-    FactoryGirl.build(:course, desc: nil).should_not be_valid
-  end
-  it "is invalid without a term" do
-    FactoryGirl.build(:course, term: nil).should_not be_valid
-  end
+
 end
