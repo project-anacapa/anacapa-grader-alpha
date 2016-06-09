c231f8bb5b7e3e1e65817f209de2e18df857c669
Baby Pepper <royxagain@gmail.com>
2016-06-08 22:07:53 -0700

    generate course model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160429235336) do
+ActiveRecord::Schema.define(version: 20160609050607) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "dept"
+    t.string   "num"
+    t.string   "desc"
+    t.string   "term"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "roles", force: :cascade do |t|
     t.string   "name"
