9db56b1a24b63a068209fcbcdb7b70924fc48deb
Baby Pepper <royxagain@gmail.com>
2016-06-08 22:30:42 -0700

    pass the rest of the test

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,10 @@
 class Course < ActiveRecord::Base
+  validates :dept, :presence => true
+  validates :num, :presence => true
+  validates :desc, :presence => true
+  validates :term, :presence => true
+
+  def course_slug
+    return [self.dept, self.num, self.term].join "_"
+  end
 end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -4,8 +4,23 @@ RSpec.describe Course, type: :model do
   it "has a valid factory" do
     FactoryGirl.create(:course).should be_valid
   end
-  it "is invalid without a department"
-  it "is invalid without a number"
-  it "is invalid without a description"
-  it "is invalid without a term"
+
+  describe "invalid initilization" do
+    it "is invalid without a department" do
+      FactoryGirl.build(:course, dept: nil).should_not be_valid
+    end
+    it "is invalid without a number" do
+      FactoryGirl.build(:course, num: nil).should_not be_valid
+    end
+    it "is invalid without a description" do
+      FactoryGirl.build(:course, desc: nil).should_not be_valid
+    end
+    it "is invalid without a term" do
+      FactoryGirl.build(:course, term: nil).should_not be_valid
+    end
+  end
+
+  it "should return the string in correct format" do
+    FactoryGirl.create(:course).course_slug.should eq("Computer Science_170_S16")
+  end
 end
