a3c44fe4ab987cc5ef82a140dcf089d17c9795ee
Baby Pepper <royxagain@gmail.com>
2016-06-08 22:24:25 -0700

    pass a basic factory test

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
-  it "has a valid factory"
+  it "has a valid factory" do
+    FactoryGirl.create(:course).should be_valid
+  end
   it "is invalid without a department"
   it "is invalid without a number"
   it "is invalid without a description"
