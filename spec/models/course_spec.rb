615ba9f81f586ab06d99da5b27275806d76e6bdb
Baby Pepper <royxagain@gmail.com>
2016-06-08 22:19:36 -0700

    add pending test

---
+++ spec/models/course_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Course, type: :model do
+  it "has a valid factory"
+  it "is invalid without a department"
+  it "is invalid without a number"
+  it "is invalid without a description"
+  it "is invalid without a term"
+end
